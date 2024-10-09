d74597f51fc8aa6cf34beceb85f6d0d787e79695
lai2-24 <lai2-24@itismeucci.com>
2024-10-09 08:46:51 +0200

    gitignore added

--- src/main/java/it/itismeucci/Main.java
+++ src/main/java/it/itismeucci/Main.java
@@ -1,7 +1,15 @@
 package it.itismeucci;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello world!");
+        ServerSocket ss = new ServerSocket(5673);
+        Socket mys = ss.accept();
+
+        System.out.println("qualcuno si è collegato");
     }
 }
